94507637c24523ca115bba7b95c9bec63e3b90a0
ZMURIK1 <161632129+ZMURIK1@users.noreply.github.com>
2025-02-07 18:50:34 +0300

    First comit

---
+++ calculator/Calculator/Calculator/Calculator.cpp
@@ -0,0 +1,45 @@
+#include<iostream> // For using cout, cin and so on
+
+void main() {
+	setlocale(LC_ALL, "Rus"); // For Russian language
+
+	while (true) {
+		// On lines 7 - 9 i created var for continue or break
+		int futureAction;
+		std::cout << "Input 1 for continue, input 0 for break: ";
+		std::cin >> futureAction;
+
+		// On lines 11 - 13 i checing var
+		if (futureAction == 0) {
+			break;	
+		}
+
+		// On lines 18 - 20 i initialize vars for expression
+		int firstNum;
+		int secondNum;
+		char action = ' ';
+
+		// On lines 23 - 25 i created var with expression
+		std::string expression;
+		std::cout << "Input expression: "; 
+		std::cin >> std::ws >> firstNum >> action >> secondNum;
+
+		switch (action)
+		{
+		case '+':
+			std::cout << firstNum << " + " << secondNum << " = " << firstNum + secondNum << "\n";
+			break;
+		case '-':
+			std::cout << firstNum << " - " << secondNum << " = " << firstNum - secondNum << "\n";
+			break;
+		case '*':
+			std::cout << firstNum << " * " << secondNum << " = " << firstNum * secondNum << "\n";
+			break;
+		case '/':
+			std::cout << firstNum << " / " << secondNum << " = " << firstNum / secondNum << "\n";
+			break;
+		default:
+			break;
+		}
+	}
+}
